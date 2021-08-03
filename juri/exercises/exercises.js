379d36236e7abe35917a58720e216b6ab577a298
Grogu H <betteraskme@protonmail.com>
2021-08-04 01:25:21 +0300

    feat(exercises): add 3 basic exercises

---
+++ juri/exercises/exercises.js
@@ -0,0 +1,27 @@
+/**
+ * Function checks if a "str" supplied is a palindrome
+ * @param {string} str String to verify
+ * @returns {boolean} True if "str" is a palindrome
+ */
+export function isPalindrome(str) {
+  // TODO
+}
+
+/**
+ * Returns maximum number from a "arrayOfNumbers" array
+ * @param {Array<number>} arrayOfNumbers
+ * @returns {number} Maximum number from array
+ */
+export function maxNumber(arrayOfNumbers) {
+  // TODO
+}
+
+/**
+ * Returns reverted string
+ * (e.g. if "str" is "Hello" the function returns "olleH").
+ * @param {string} str String to revert
+ * @returns {string} Reverted string
+ */
+export function reverse(str) {
+  // TODO
+}
---
+++ nata/exercises/exercises.test.js
@@ -0,0 +1,53 @@
+const { isPalindrome, maxNumber, reverse } = require("./exercises.js");
+
+describe("Palindrome Suite", function () {
+  it("should return true for word 'kazak'", function () {
+    expect(isPalindrome("kazak")).toBe(true);
+  });
+
+  it("should return false for word 'vasya'", function () {
+    expect(isPalindrome("vasya")).toBe(false);
+  });
+
+  it("should return false for empty word", function () {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  it("should return false for null and undefined", function () {
+    expect(isPalindrome(null)).toBe(false);
+    expect(isPalindrome(undefined)).toBe(false);
+  });
+});
+
+describe("Max Number Suite", function () {
+  it("should return 5 from an array [1, 2, 5, 4, 3]", function () {
+    expect(maxNumber([1, 2, 5, 4, 3])).toBe(5);
+  });
+
+  it("should return 5 from an array [-1, 2, 5, -4, 3]", function () {
+    expect(maxNumber([-1, 2, 5, -4, 3])).toBe(5);
+  });
+
+  it("should return 5 from an array [-1, 2, 5, -4, 3]", function () {
+    expect(maxNumber([-1, 2, 5, -4, 3])).toBe(5);
+  });
+});
+
+describe("Reverse Suite", function () {
+  it("should return 'kazak' for word 'kazak'", function () {
+    expect(reverse("kazak")).toBe("kazak");
+  });
+
+  it("should return olleH for word 'Hello'", function () {
+    expect(reverse("Hello")).toBe("olleH");
+  });
+
+  it("should return empty string for empty input", function () {
+    expect(reverse("")).toBe("");
+  });
+
+  it("should return empty string for null and undefined", function () {
+    expect(reverse(null)).toBe("");
+    expect(reverse(undefined)).toBe("");
+  });
+});
