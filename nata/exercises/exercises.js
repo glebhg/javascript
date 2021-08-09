133a001fa84b4d63924088566c8646242cfb156a
NatalliaMil <milkonatallia@gmail.com>
2021-08-09 20:37:41 +0300

    feat(exercise): solved exercises with "for" loop.

--- nata/exercises/exercises.js
+++ nata/exercises/exercises.js
@@ -4,7 +4,15 @@
  * @returns {boolean} True if "str" is a palindrome
  */
 export function isPalindrome(str) {
-  // TODO
+  if (str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+    }
+    return newString === str;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -13,7 +21,13 @@ export function isPalindrome(str) {
  * @returns {number} Maximum number from array
  */
 export function maxNumber(arrayOfNumbers) {
-  // TODO
+  var max = arrayOfNumbers[0];
+  for (var i = 1; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] > max) {
+      max = arrayOfNumbers[i];
+    }
+  }
+  return max;
 }
 
 /**
@@ -23,5 +37,13 @@ export function maxNumber(arrayOfNumbers) {
  * @returns {string} Reverted string
  */
 export function reverse(str) {
-  // TODO
+  if (str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+    }
+    return newString;
+  } else {
+    return (str = "");
+  }
 }
