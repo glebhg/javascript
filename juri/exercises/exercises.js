cd0407140f79815f09ade9466cdc13284645480c
yogahub <jurilbr@gmail.com>
2021-08-18 00:17:25 +0300

    fix

--- juri/exercises/exercises.js
+++ juri/exercises/exercises.js
@@ -3,25 +3,46 @@
  * @param {string} str String to verify
  * @returns {boolean} True if "str" is a palindrome
  */
-export function isPalindrome(str) {
-  // TODO
-}
 
-/**
- * Returns maximum number from a "arrayOfNumbers" array
- * @param {Array<number>} arrayOfNumbers
- * @returns {number} Maximum number from array
- */
-export function maxNumber(arrayOfNumbers) {
-  // TODO
-}
 
-/**
- * Returns reverted string
- * (e.g. if "str" is "Hello" the function returns "olleH").
- * @param {string} str String to revert
- * @returns {string} Reverted string
- */
-export function reverse(str) {
+ export function isPalindrome(str) {
   // TODO
-}
+    if (str) {
+    return str === str.split('').reverse().join('');
+    }else {
+      return false;
+    }
+    
+  }
+
+
+  /**
+   * Returns maximum number from a "arrayOfNumbers" array
+   * @param {Array<number>} arrayOfNumbers
+   * @returns {number} Maximum number from array
+   */
+  export function maxNumber(arrayOfNumbers) {
+    var maxNumber = arrayOfNumbers[0];
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+      if (maxNumber < arrayOfNumbers[i]) {
+        maxNumber = arrayOfNumbers[i];
+      }
+
+    }
+    return maxNumber;
+  }
+
+  /**
+   * Returns reverted string
+   * (e.g. if "str" is "Hello" the function returns "olleH").
+   * @param {string} str String to revert
+   * @returns {string} Reverted string
+   */
+  export function reverse(str) {
+    if (str) {
+      return (str = str.split('').reverse().join(''));
+    } else {
+      return (str = '');
+    }
+
+  }
