cb4c51a5cdf0bd2d20895e059edb47b8ebfcb48d
yogahub <jurilbr@gmail.com>
2021-08-17 11:33:30 +0300

    feach(gallery): add select imgs
    -select images;
    -added buttons blackout and blacklight;

--- juri/gallery-start/main.js
+++ juri/gallery-start/main.js
@@ -4,28 +4,27 @@ var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 var displayedImageHandler = function (e) {
+  e.stopPropagation();
   displayedImage.src = e.target.src;
  }; 
- 
 
-for (var i = 1; i<5; i++) {
+document.querySelector('.thumb-bar-par').addEventListener('click',displayedImageHandler);
+
+for (var i = 1; i<=5; i++) {
 var newImage = document.createElement('img');
 newImage.setAttribute('src','images/pic'+ i +'.jpg');
 thumbBar.appendChild(newImage);
-//newImage.addEventListener('click', displayedImageHandler);
 }
 
-var thumbBarPar = e.stopPropagation();
-
 btn.onclick = function() {
-   if (btn === "dark") {
+   if (btn.getAttribute('class') === "dark") {
    btn.setAttribute('class', 'light');
    btn.textContent = "Cветлее";
-   overlay.style.backgroundColor = "rgba (0,0,0,0.5)";
+   overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
    } else {
    btn.setAttribute('class', 'dark');
    btn.textContent = "Темнее";
-   overlay.style.backgroundColor = "rgba (0,0,0,0)";
+   overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
 };
 
