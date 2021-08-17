34163e5f6b5a7839012da6d4a8b26586ba52d91b
yogahub <jurilbr@gmail.com>
2021-08-17 12:09:56 +0300

    feat(gallery): add select imgs
    -select images;
    -added buttons blackout and blacklight.

--- juri/gallery-start/main.js
+++ juri/gallery-start/main.js
@@ -6,25 +6,25 @@ var overlay = document.querySelector('.overlay');
 var displayedImageHandler = function (e) {
   e.stopPropagation();
   displayedImage.src = e.target.src;
- }; 
+};
 
-document.querySelector('.thumb-bar-par').addEventListener('click',displayedImageHandler);
+document.querySelector('.thumb-bar-par').addEventListener('click', displayedImageHandler);
 
-for (var i = 1; i<=5; i++) {
-var newImage = document.createElement('img');
-newImage.setAttribute('src','images/pic'+ i +'.jpg');
-thumbBar.appendChild(newImage);
+for (var i = 1; i <= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
 }
 
-btn.onclick = function() {
-   if (btn.getAttribute('class') === "dark") {
-   btn.setAttribute('class', 'light');
-   btn.textContent = "Cветлее";
-   overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-   } else {
-   btn.setAttribute('class', 'dark');
-   btn.textContent = "Темнее";
-   overlay.style.backgroundColor = "rgba(0,0,0,0)";
+btn.onclick = function () {
+  if (btn.getAttribute('class') === "dark") {
+    btn.setAttribute('class', 'light');
+    btn.textContent = "Cветлее";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = "Темнее";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
 };
 
